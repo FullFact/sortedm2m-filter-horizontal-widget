7c408298526100ffae003cf2b74e6374138d9b6f
Jenna Howe <jenna.howe@live.co.uk>
2017-09-27 15:39:06 +0100

    Move new js and css into more sensible places

--- sortedm2m_filter_horizontal_widget/static/sortedm2m_filter_horizontal_widget/OrderedSelectFilter.js
+++ sortedm2m_filter_horizontal_widget/static/sortedm2m_filter_horizontal_widget/OrderedSelectFilter.js
@@ -168,6 +168,32 @@ var OrderedSelectFilter = {
         }
         // Initial icon refresh
         OrderedSelectFilter.refresh_icons(field_id);
+
+        // Convert search function to use a django-autocomplete-light view instead of rendering all content from the get-go 
+        // This is shoved in here to make sure it happens after the widget has been constructed
+        var fieldName = 'id_debate_1_content';
+
+        $('#' + fieldName + '_input').focus(function(e) {
+            var searchField = $(e.currentTarget);
+            var url = searchField.attr("data-autocomplete-light-url");
+
+            var resultsShowing = $('#' + fieldName + '_results').length > 0;
+
+            if (!resultsShowing) {
+                $.getJSON( url, function( data ) {
+                    var searchResultsBox = $(document.createElement('div'))
+                        .addClass('search-results')
+                        .attr('id', fieldName + '_results');
+
+                    data.results.forEach(function(content) {
+                        var result = $(document.createElement('option')).text(content.text);
+                        result.attr('value', content.id);
+                        searchResultsBox.append(result);
+                    });
+                    searchField.after(searchResultsBox);
+                });
+            }
+        });
     },
     refresh_icons: function(field_id) {
         var from = $('#' + field_id + '_from');
